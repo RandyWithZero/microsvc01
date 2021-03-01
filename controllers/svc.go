5acbd685a75ec7c6350fc061758a02ad2725a0a1
wangsq <wangsq@tongtech.com>
2021-03-01 18:39:58 +0800

    update

--- controllers/svc.go
+++ controllers/svc.go
@@ -4,20 +4,23 @@ import (
 	"github.com/astaxie/beego"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 const (
-	microSvcDb01Url ="http://svcdb:9091/api/svcdb"
+	microSvcDb01Url = "http://svcdb:9091/api/svcdb"
 )
 
 type MicroSVC01Controller struct {
 	beego.Controller
 }
+
 func (c *MicroSVC01Controller) Get() {
 	response, _ := http.Get(microSvcDb01Url)
 	defer response.Body.Close()
 	all, _ := ioutil.ReadAll(response.Body)
-	msg :="--->调用svc01"
 	msg2 := string(all)
-	c.Ctx.WriteString(msg+msg2)
+	getEnv := os.Getenv("HOSTNAME")
+	msg := "--->调用svc01" + "[" + getEnv + "]"
+	c.Ctx.WriteString(msg + msg2)
 }
